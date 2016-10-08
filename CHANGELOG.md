6365d12d4cd51b1c2de76858772dad1cffffead7
Andrew Duthie <andrew@andrewduthie.com>
2016-10-07 21:26:28 -0400

    Emit chunk name as slug (no path separators)

---
+++ CHANGELOG.md
@@ -0,0 +1,7 @@
+#### v0.1.1 (2016-10-07)
+
+- Fix: Emit chunk name as slug (no path separators)
+
+#### v0.1.0 (2016-10-06)
+
+- Initial release
--- index.js
+++ index.js
@@ -1,9 +1,18 @@
+/**
+ * External dependencies
+ */
+const kebabCase = require( 'lodash.kebabcase' );
+
 module.exports = function( babel ) {
 	const t = babel.types;
 
 	return {
 		visitor: {
 			JSXAttribute: function( path ) {
+				// We only transform the require prop on AsyncLoad components.
+				// The component could have been imported under a different
+				// name, but tracking the identifier to the import would add
+				// complexity to the parsing. In other words, I'm lazy.
 				const parent = path.parentPath.parent;
 				if ( 'AsyncLoad' !== parent.openingElement.name.name ) {
 					return;
@@ -19,6 +28,9 @@ module.exports = function( babel ) {
 					return;
 				}
 
+				// Replace prop string with function which, when invoked, calls
+				// asyncRequire. The asyncRequire call is transformed by the
+				// CallExpression visitor in this plugin
 				const newValue = t.jSXExpressionContainer(
 					t.functionExpression( null, [ t.identifier( 'callback' ) ], t.blockStatement( [
 						t.expressionStatement( t.callExpression( t.identifier( 'asyncRequire' ), [
@@ -40,25 +52,36 @@ module.exports = function( babel ) {
 					return;
 				}
 
+				// Determine async from Babel plugin options
 				const isAsync = state.opts.async;
 
+				// In both asynchronous and synchronous case, we'll finish by
+				// calling reqquire on the loaded module
 				let requireCall = t.callExpression( t.identifier( 'require' ), [ argument ] );
+
+				// If a callback was passed as an argument, wrap it as part of
+				// the transformation
 				const callback = path.node.arguments[ 1 ];
 				if ( callback ) {
 					requireCall = t.callExpression( callback, [ requireCall ] );
 				}
 
 				if ( isAsync ) {
+					// Generate a chunk name based on the require path
+					const chunkName = 'async-load-' + kebabCase( argument.value );
+
+					// Transform to asynchronous require.ensure
 					path.replaceWith(
 						t.callExpression( t.memberExpression( t.identifier( 'require' ), t.identifier( 'ensure' ) ), [
 							argument,
 							t.functionExpression( null, [ t.identifier( 'require' ) ], t.blockStatement( [
 								t.expressionStatement( requireCall )
 							] ) ),
-							t.stringLiteral( 'async-load-' + argument.value )
+							t.stringLiteral( chunkName )
 						] )
 					);
 				} else {
+					// Transform to synchronous require
 					path.replaceWith( requireCall );
 				}
 			}
